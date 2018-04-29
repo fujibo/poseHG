14816ba919f162f47b16e86fc94864ecee986f36
fujibo <kt.tsubo@gmail.com>
2018-04-30 00:18:28 +0900

    eval debugged

--- chainer/eval.py
+++ chainer/eval.py
@@ -10,6 +10,8 @@ import os
 
 from chainer.dataset import concat_examples
 
+from tqdm import tqdm
+
 
 def pckh_score(keypoint_true, keypoint_pred, idx, scale):
     """evaluation of keypoint
@@ -26,7 +28,7 @@ def pckh_score(keypoint_true, keypoint_pred, idx, scale):
     dist = np.sqrt(np.sum((keypoint_true - keypoint_pred) ** 2, axis=2))
 
     # normalized by the head size
-    dist = dist / scale
+    dist = dist / scale[:, np.newaxis]
     n_joints = idx.shape[1]
     correct = [np.sum(dist[idx[:, i], i] <= 0.5) for i in range(n_joints)]
     correct = np.array(correct)
@@ -42,41 +44,47 @@ def evaluate(model, dataset, device=-1):
     corrects = list()
     counts = list()
 
-    for batch in data_iter:
-        img, label, idx, scale = concat_examples(batch)
+    for it, batch in enumerate(data_iter):
 
-        shape = list()
-        img_resized = list()
-        for i in range(len(batch)):
-            shape.append(img.shape)
-            img_ = transforms.resize(img[i], (256, 256))
-            img_resized.append(img_)
-
-        img_resized = np.array(img_resized)
+        img, label, idx, scale, shape = concat_examples(batch)
         if device >= 0:
-            img_resized = cuda.to_gpu(img_resized)
+            img = cuda.to_gpu(img)
 
-        # (N, 3, 256, 256) -> (N, 16, 64, 64)
-        _output, output = model(img_resized)
-        output = cuda.to_cpu(output.array)
+        with chainer.no_backprop_mode():
+            # (N, 3, 256, 256) -> (N, 16, 64, 64)
+            _output, output = model(img)
 
-        C, H, W = output.shape
+        output = output.array
+        N, C, H, W = output.shape
 
-        # (16, 64, 64) -> (16, )
-        output = output.reshape(C, -1).argmax(axis=1)
-        keypoint = np.unravel_index(output, (H, W))
-        keypoint = np.array(keypoint).T
-        keypoint = transforms.resize_point(keypoint, (H, W), img.shape[1:])
+        keypoints = list()
+        # (N, 16, 64, 64) -> (N, 16, 2)
+        for i in range(N):
+            # (16, 64, 64) -> (16, -1)
+            out_reshaped = output[i].reshape(C, -1).argmax(axis=1)
+            out_reshaped = cuda.to_cpu(out_reshaped)
+            keypoint = np.unravel_index(out_reshaped, (H, W))
+            # (2, 16) -> (16, 2)
+            keypoint = np.array(keypoint).T
+            keypoint = transforms.resize_point(keypoint, (H, W), shape[i][1:])
+            keypoints.append(keypoint)
 
-        correct, count = pckh_score(label, keypoint, idx, scale)
+        else:
+            keypoints = np.array(keypoints)
+
+        correct, count = pckh_score(label, keypoints, idx, scale)
         corrects.append(correct)
         counts.append(count)
+
+        # DEBUG
+        # if it == 2:
+        #     break
 
     corrects = np.sum(corrects, axis=0)
     counts = np.sum(counts, axis=0)
     # Head, Shoulder, Elbow, Wrist, Hip, Knee, Ankle
-    joints = {'head': (9, ), 'shoulder': (12, 13), 'elbow': (11, 14),
-              'wrist': (10, 15), 'hip': (2, 3), 'knee': (1, 4), 'ankle': (0, 5)}
+    joints = {'head': [9, ], 'shoulder': [12, 13], 'elbow': [11, 14],
+              'wrist': [10, 15], 'hip': [2, 3], 'knee': [1, 4], 'ankle': [0, 5]}
 
     scores = dict()
     for key, value in joints.items():
@@ -102,10 +110,9 @@ def main():
         cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
 
-    chainer.config.train = False
-
     dataset = MPIIDataset(split='val')
-    scores = evaluate(model, dataset, args.gpu)
+    with chainer.using_config('train', False):
+        scores = evaluate(model, dataset, args.gpu)
     print(scores)
 
 
