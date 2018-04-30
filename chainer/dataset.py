7dc16096901c08980f80e8bc01b8c4e5305799e0
fujibo <kt.tsubo@gmail.com>
2018-04-30 17:20:50 +0900

    considering multi person

--- chainer/dataset.py
+++ chainer/dataset.py
@@ -28,11 +28,14 @@ class MPIIDataset(DatasetMixin):
         self._split = split
 
         fname = f'{self._root}/annotations/mpii_human_pose_v1_u12_1.mat'
-        paths, keypoints, head_sizes =  read_mpii_annots(fname, split)
+        paths, keypoints, head_sizes, centers, scales =  read_mpii_annots(fname, split)
 
         self.paths = paths
         self.keypoints = keypoints
         self.head_sizes = head_sizes
+
+        self.centers = centers
+        self.scales = scales
 
     def __len__(self):
         return len(self.paths)
@@ -42,12 +45,28 @@ class MPIIDataset(DatasetMixin):
         img = utils.read_image(img_path)
 
         keypoint = self.keypoints[i]
+        center = self.centers[i]
+        scale = self.scales[i]
+
         if self._split == 'train':
-            img, label, idx = preprocess(img, keypoint)
+            img, label, idx = preprocess(img, keypoint, center, scale)
             return img, label, idx
 
         # 'val' or 'test'
         else:
+            shape = img.shape
+
+            xmin, xmax = center[0] - scale/2, center[0] + scale/2
+            ymin, ymax = center[1] - scale/2, center[1] + scale/2
+
+            # truncate
+            xmin = int(max(0, xmin))
+            ymin = int(max(0, ymin))
+            xmax = int(min(shape[2], xmax))
+            ymax = int(min(shape[1], ymax))
+
+            # croping
+            img = img[:,ymin:ymax, xmin:xmax]
             point = list()
             idx = list()
             for i, (x, y) in enumerate(zip(keypoint['x'], keypoint['y'])):
@@ -57,16 +76,15 @@ class MPIIDataset(DatasetMixin):
                     idx.append(False)
 
                 else:
-                    point.append((y, x))
+                    point.append((y-ymin, x-xmin))
                     idx.append(True)
 
             point = np.array(point)
             idx = np.array(idx)
 
-            shape = img.shape
             img = transforms.resize(img, (256, 256))
 
-            return img, point, idx, self.head_sizes[i], shape
+            return img, point, idx, self.head_sizes[i], (ymax-ymin, xmax-xmin)
 
 
 def read_mpii_annots(fname, split):
@@ -97,6 +115,9 @@ def read_mpii_annots(fname, split):
     paths = list()
     keypoints = list()
     head_size = list()
+
+    centers = list()
+    scales = list()
 
     for data_idx in np.where(arr.img_train == 1)[0]:
         annot = arr.annolist[data_idx]
@@ -139,28 +160,47 @@ def read_mpii_annots(fname, split):
             SC_BIAS = 0.8 * 0.75
             head_size.append(SC_BIAS * np.linalg.norm([an.x2 - an.x1, an.y2 - an.y1]))
 
+            center = [(an.x1 + an.x2)/2, (an.y1 + an.y2)/2]
+            scale = an.scale * 200
+
             paths.append(path)
             keypoints.append(keypoint)
+            centers.append(center)
+            scales.append(scale)
 
     head_size = np.array(head_size)
     print(len(paths))
     assert len(paths) == len(keypoints)
 
-    return paths, keypoints, head_size
+    return paths, keypoints, head_size, centers, scales
 
 
-def preprocess(img, keypoint):
+def preprocess(img, keypoint, center, scale):
     """preprocess image and keypoint
     Args:
         img: CxHxW
         keypoint: dict {'x': [None]*16, 'y': [None]*16, 'visible': [None]*16}
+        center: [x, y] center of a person
+        scale: float, size of a person
     Returns:
         img: shape Cx256x256, (not copied)
         heatmap: shape 16x64x64
         indices: index available
     """
     # image processing
-    shape = img.shape[1:]
+    shape = img.shape
+
+    xmin, xmax = center[0] - scale / 2, center[0] + scale / 2
+    ymin, ymax = center[1] - scale / 2, center[1] + scale / 2
+
+    # truncate
+    xmin = int(max(0, xmin))
+    ymin = int(max(0, ymin))
+    xmax = int(min(shape[2], xmax))
+    ymax = int(min(shape[1], ymax))
+
+    # croping
+    img = img[:, ymin:ymax, xmin:xmax]
     img = transforms.resize(img, (256, 256))
 
     # data augmenation
@@ -176,13 +216,13 @@ def preprocess(img, keypoint):
     heatmap = np.zeros((16, 64, 64), dtype=np.float32)
     indices = list()
     for i, (x, y) in enumerate(zip(keypoint['x'], keypoint['y'])):
-        point = np.array((y, x))
 
         if None in (x, y):
             indices.append(False)
 
         else:
-            point_resized = transforms.resize_point(point[np.newaxis], shape, (64, 64))
+            point = np.array((y - ymin, x - xmin))
+            point_resized = transforms.resize_point(point[np.newaxis], shape[1:], (64, 64))
             y, x = point_resized[0].astype(np.int64)
             y, x = min(y, 63), min(x, 63)
 
@@ -201,3 +241,4 @@ def preprocess(img, keypoint):
 
 if __name__ == '__main__':
     dataset = MPIIDataset()
+    dataset.get_example(15)
